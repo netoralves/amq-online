baba98c9d571f77ea1ee94b1db6de6341fce325e
adm.franciscorm <fneto@alltechsolucoes.com.br>
2022-02-01 10:57:18 -0300

    Create type uses for brokered and standard with or not specific cm

--- build_java/src/main/resources/openshift/create_demo_app.sh
+++ build_java/src/main/resources/openshift/create_demo_app.sh
@@ -3,6 +3,14 @@ set -x
 AMQ_ONLINE_INFRA_PROJ=amq-online-infra
 AMQ_ONLINE_DEMO_PROJ=amqp-demo
 
+#OPTIONS: demo-objects-standard.yaml / demo-objects-standard-external.yaml / demo-objects-brokered.yaml / demo-objects-brokered-external.yaml
+AMQ_ONLINE_PROFILE=demo-objects-standard-external.yaml
+
+
+#OPTIONS: if use demo-objects-standard* = demo-standard-space / if use demo-objects-brokered* = demo-brokered-space
+AMQ_ONLINE_ADDRESSSPACE=demo-standard-space
+
+
 output=$(oc get project ${AMQ_ONLINE_DEMO_PROJ})
 
 if [ -z "${output}" ]; then
@@ -12,10 +20,10 @@ fi
 
 echo "Proceeding"
 
-oc apply -f src/main/resources/openshift/demo-objects.yaml -n ${AMQ_ONLINE_DEMO_PROJ}
+oc apply -f src/main/resources/openshift/${AMQ_ONLINE_PROFILE} -n ${AMQ_ONLINE_DEMO_PROJ}
 
 oc get routes console -o jsonpath={.spec.host} -n ${AMQ_ONLINE_INFRA_PROJ}
 
 sleep 5
 
-oc get addressspace demo-space -o 'jsonpath={.status.endpointStatuses[?(@.name=="messaging")].externalHost}' -n ${AMQ_ONLINE_DEMO_PROJ}
+oc get addressspace ${AMQ_ONLINE_ADDRESSSPACE} -o 'jsonpath={.status.endpointStatuses[?(@.name=="messaging")].externalHost}' -n ${AMQ_ONLINE_DEMO_PROJ}
